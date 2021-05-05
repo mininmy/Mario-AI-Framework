3a9e107ef207f883788070fdad68ae06e47e3ffb
mykha <mykhailo.minin@gmail.com>
2021-05-05 05:41:49 +0300

    with LSTM

--- src/GenerateLevel.java
+++ src/GenerateLevel.java
@@ -22,10 +22,10 @@ public class GenerateLevel {
     }
 
     public static void main(String[] args) {
-        MarioLevelGenerator generator = new levelGenerators.notch.LevelGenerator();
-        String level = generator.getGeneratedLevel(new MarioLevelModel(150, 16), new MarioTimer(5 * 60 * 60 * 1000));
+        MarioLevelGenerator generator = new levelGenerators.network.LevelGenerator();
+        String level = generator.getGeneratedLevel(new MarioLevelModel(1, 16), new MarioTimer(5* 60 * 60 * 1000));
         MarioGame game = new MarioGame();
         // printResults(game.playGame(level, 200, 0));
-        printResults(game.runGame(new agents.robinBaumgarten.Agent(), level, 20, 0, true));
-    }
+        printResults(game.runGame(new agents.robinBaumgarten.Agent(), level, 1, 0, true));
+       }
 }
--- src/PlayLevel.java
+++ src/PlayLevel.java
@@ -34,6 +34,7 @@ public class PlayLevel {
     public static void main(String[] args) {
         MarioGame game = new MarioGame();
         // printResults(game.playGame(getLevel("../levels/original/lvl-1.txt"), 200, 0));
-        printResults(game.runGame(new agents.robinBaumgarten.Agent(), getLevel("./levels/original/lvl-1.txt"), 20, 0, true));
+        //printResults(game.runGame(new agents.robinBaumgarten.Agent(), getLevel("./levels/original/lvl-1.txt"), 20, 0, true));
+        printResults(game.runGame(new agents.robinBaumgarten.Agent(), getLevel("./levels/my/filename.txt"), 20, 0, true));
     }
 }
